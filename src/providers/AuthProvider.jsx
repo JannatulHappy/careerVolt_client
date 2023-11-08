d6714f45f7b45632f494e494b095181715984f27
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-08 21:16:37 +0600

    fixed token issue

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -55,7 +55,7 @@ const AuthProvider = ({ children }) => {
       // if user exists then issue a token
       if (currentUser) {
         axios
-          .post("http://localhost:5000/jwt", loggedUser, {
+          .post("https://career-volt-server.vercel.app/jwt", loggedUser, {
             withCredentials: true,
           })
           .then((res) => {
@@ -64,7 +64,7 @@ const AuthProvider = ({ children }) => {
       } else {
         // remove token after logout
         axios
-          .post("http://localhost:5000/logout", loggedUser, {
+          .post("https://career-volt-server.vercel.app/logout", loggedUser, {
             withCredentials: true,
           })
           .then((res) => {
